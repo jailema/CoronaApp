b52a270c246121993acac3c5e72ed6e851020b58
Danielooooo <daniel@livecareusa.com>
2021-03-02 14:24:35 -0500

    First commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:my_first_app/home.dart';
+import 'package:app_corona/home.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
